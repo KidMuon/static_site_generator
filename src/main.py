c8adba5606e9edb01464ca3a6d0645bf36db05ea
Dominique Berry <Dominique174466@gmail.com>
2024-03-28 14:09:59 -0400

    Removing lines not needed in main.

--- src/main.py
+++ src/main.py
@@ -5,9 +5,6 @@ import os
 def main():
     recursive_copy('static', 'public')
     generate_page('content/index.md', 'template.html', 'public/index.html')
-    #document = """# Heading text \n\n* Quick list `code` and \n* Quick list2"""
-    #html = markdown_to_htmlnode(document).to_html()
-    #print(html)
 
 
 if __name__ == "__main__":
